555bbbb518b9b0927e52f82e06e6be8fb7122036
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2019-06-11 21:42:24 +0530

    Update openjdk8.sh

--- version/openjdk8.sh
+++ version/openjdk8.sh
@@ -8,3 +8,5 @@ apt-get install -y openjdk-"$OPEN_JDK"-jdk
 add-apt-repository ppa:maarten-fonville/ppa
 apt-get update
 apt-get install -y icedtea-"$ICEDTEA"-plugin
+
+update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-8-openjdk-amd64/bin/java 1
