da79e3f9dca8c7bcf64b2e65b017e140fd39fa9b
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-06-11 14:05:40 +0530

    Update _java.sh

--- test/_java.sh
+++ test/_java.sh
@@ -1,12 +1,18 @@
 #!/bin/bash -e
 
 echo "============================ JDK versions ==============================="
+echo "update-alternatives --list java"
+update-alternatives --list java
+printf "\n\n"
 
-
-shipctl jdk set openjdk8
+echo "update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java"
+update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+printf "\n"
 java -version
 printf "\n"
 
-shipctl jdk set openjdk11
+echo "update-alternatives --set java /usr/lib/jvm/java-11-openjdk-amd64/jre/bin/java"
+update-alternatives --set java /usr/lib/jvm/java-11-openjdk-amd64/jre/bin/java
+printf "\n"
 java -version
 printf "\n"
